69f196dd06082c5bc5ec6ff4adc5270256bfd27d
Campos Ferreira <lucascampos_ferreira.ext@siemens.com>
2020-11-24 10:35:46 -0300

    inclusão de uma query para pontos analogicos

--- lista_de_pontos.py
+++ lista_de_pontos.py
@@ -5,7 +5,7 @@ from pybrsql import Pybrsql
 path = os.environ['BD']
 brsql = Pybrsql(ct_or_path=path, source='xdr')
 
-query = '''select 
+QUERY_PONTOS_DIGITAIS = '''select 
 pds.id as id,
 pds.nome as nome,
 pds.idocr as ocr, 
@@ -19,12 +19,34 @@ left join tcl
 on pds.a_tcl = tcl.br_rowid
 where pds.idlia !=''
 '''
+brsql.consulta_bd(QUERY_PONTOS_DIGITAIS)
+pontos_digitais = brsql.tocsv()
 
+QUERY_PONTOS_ANALOGICOS = '''select
+pas.id as id,
+pas.nome as nome,
+pas.idocr as ocr, 
+pas.idlia as rele,
+COALESCE(tcl.id, 'NLCL') as calculo,
+COALESCE(paf.id, '') as end
+FROM pas
+left join paf
+on pas.a_paf = paf.br_rowid
+left join tcl
+on pas.a_tcl = tcl.br_rowid
+where pas.idlia !=''
+'''
+brsql.consulta_bd(QUERY_PONTOS_ANALOGICOS)
+pontos_analogicos = brsql.tocsv()
+
+query = '''select
+id, nome, a_tctl, idpit, idpac
+from cgs
+'''
 brsql.consulta_bd(query)
-
-output = brsql.tocsv()
+brsql.tabula()
 
 file_path = os.environ['SAGE'] + '/lista_de_pontos_siemens.csv'
 
-with open(file_path, 'write') as f:
-    f.writelines(output)
+#with open(file_path, 'write') as f:
+#    f.writelines(pontos_digitais)
