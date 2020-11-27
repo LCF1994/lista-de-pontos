13482c49e3f7f9dda5d88f9b0e49eb708f595bd9
Campos Ferreira <lucascampos_ferreira.ext@siemens.com>
2020-11-27 10:13:04 -0300

    Implementação de funções para melhor legibilidade do código

--- lista_de_pontos.py
+++ lista_de_pontos.py
@@ -3,67 +3,33 @@ from pybrsql import Pybrsql
 import json
 
 
-#path = os.environ['BD']
-path = '/export/home/sagetr1/sage/config/mt_eln/bd'
-brsql = Pybrsql(ct_or_path=path, source='xdr')
-
-QUERY_PONTOS_DIGITAIS = '''select
-pds.id as id,
-pds.nome as nome,
-pds.idocr as ocr,
-pds.idlia as rele,
-COALESCE(tcl.id, 'NLCL') as calculo,
-COALESCE(pdf.id, '') as end
-FROM pds
-left join pdf
-on pds.a_pdf = pdf.br_rowid
-left join tcl
-on pds.a_tcl = tcl.br_rowid
-where pds.idlia !=''
-'''
-brsql.consulta_bd(QUERY_PONTOS_DIGITAIS)
-pontos_digitais = brsql.tocsv()
-
-QUERY_PONTOS_ANALOGICOS = '''select
-pas.id as id,
-pas.nome as nome,
-pas.idocr as ocr,
-pas.idlia as rele,
-COALESCE(tcl.id, 'NLCL') as calculo,
-COALESCE(paf.id, '') as end
-FROM pas
-left join paf
-on pas.a_paf = paf.br_rowid
-left join tcl
-on pas.a_tcl = tcl.br_rowid
-where pas.idlia !=''
-'''
-brsql.consulta_bd(QUERY_PONTOS_ANALOGICOS)
-pontos_analogicos = brsql.tocsv()
-
-QUERY_COMANDOS = '''select
-cgs.id,
-cgs.nome,
-cgs.idpac as feedback,
-tctl.dlg_close as comando_1,
-tctl.dlg_trip as comando_0,
-cgs.idpit as intertravamento,
-cgf.id as end_aquisicao,
-cgf.kconv,
-COALESCE(cgf2.id,'') as end_dist
-from cgs
-left join cgf
-on cgs.a_cgf = cgf.br_rowid
-left join cgf as cgf2
-on cgs.id = REPLACE(cgf2.kconv, "CGS= ", "")
-left join tctl
-on cgs.a_tctl = tctl.br_rowid
-'''
-brsql.consulta_bd(QUERY_COMANDOS)
-comandos = brsql.tocsv()
+# contexto = os.environ['BD']
+contexto = '/export/home/sagetr1/sage/config/mt_eln/bd'
+brsql = Pybrsql(ct_or_path=contexto, source='xdr')
 
 
-def cria_query_digital(bd):
+def query_comandos():
+    return '''select
+    cgs.id,
+    cgs.nome,
+    cgs.idpac as feedback,
+    tctl.dlg_close as comando_1,
+    tctl.dlg_trip as comando_0,
+    cgs.idpit as intertravamento,
+    cgf.id as end_aquisicao,
+    cgf.kconv,
+    COALESCE(cgf2.id,'') as end_dist
+    from cgs
+    left join cgf
+    on cgs.a_cgf = cgf.br_rowid
+    left join cgf as cgf2
+    on cgs.id = REPLACE(cgf2.kconv, "CGS= ", "")
+    left join tctl
+    on cgs.a_tctl = tctl.br_rowid
+    '''
+    
+
+def query_digital(bd):
 
     bd.consulta_bd('''
     select distinct
@@ -71,7 +37,7 @@ def cria_query_digital(bd):
     from pdd
     ''')
     
-    colunas, joins = anexa_endereco_distribuicao_digital(
+    colunas, joins = endereco_distribuicao_digital(
         json.loads(brsql.tojson()))
 
     query = '''select
@@ -93,7 +59,7 @@ def cria_query_digital(bd):
     return query
 
 
-def anexa_endereco_distribuicao_digital(lista_tdds):
+def endereco_distribuicao_digital(lista_tdds):
     coluna = ''
     join = ''
     for i, tdd in enumerate(lista_tdds):
@@ -111,7 +77,7 @@ def anexa_endereco_distribuicao_digital(lista_tdds):
     return coluna, join
 
 
-def cria_query_analogica(bd):
+def query_analogica(bd):
 
     bd.consulta_bd('''
     select distinct
@@ -119,7 +85,7 @@ def cria_query_analogica(bd):
     from pad
     ''')
     
-    colunas, joins = anexa_endereco_distribuicao_analogico(
+    colunas, joins = endereco_distribuicao_analogico(
         json.loads(brsql.tojson()))
 
     query = '''select
@@ -142,7 +108,7 @@ def cria_query_analogica(bd):
     return query
 
 
-def anexa_endereco_distribuicao_analogico(lista_tdds):
+def endereco_distribuicao_analogico(lista_tdds):
     coluna = ''
     join = ''
     for i, tdd in enumerate(lista_tdds):
@@ -160,45 +126,33 @@ def anexa_endereco_distribuicao_analogico(lista_tdds):
     return coluna, join
 
 
-brsql.consulta_bd(cria_query_digital(brsql))
+def exibe_resultados(bd, query):
+    bd.consulta_bd('select count(*) as pontos from ({})'.format(query))
+    bd.tabula()
+
+    print(bd.tojson())
+
+
+def gera_arquivo(conteudo, nome):
+    file_path = os.environ['SAGE'] + '/' + nome
+
+    with open(file_path, 'write') as f:
+        f.writelines(conteudo)
+
+
+brsql.consulta_bd(query_digital(brsql))
+pontos_digitais = brsql.tocsv()
+print('Pontos Digitais :')
 brsql.tabula()
+# gera_arquivo(pontos_digitais, 'Template_de_pontos_digitais.csv')
 
-brsql.consulta_bd('select count(*) as pontos from ({})'.format(
-    cria_query_digital(brsql)))
+brsql.consulta_bd(query_analogica(brsql))
+pontos_analogicos = brsql.tocsv()
+print('Pontos Analogicos :')
 brsql.tabula()
-print(brsql.tocsv())
-print(brsql.tojson())
+# gera_arquivo(pontos_digitais, 'Template_de_pontos_analogicos.csv')
 
-brsql.consulta_bd(cria_query_analogica(brsql))
-brsql.tabula()
-
-query = '''select 
-pdd.*, pdf.id as endereco
-from pdd
-join pdf
-on pdd.a_pdf = pdf.br_rowid
-where idpds = 'MTSY601POS'
-
-'''
-brsql.consulta_bd(query)
-brsql.tabula()
-
-query2 = '''select 
-id, nome, p_pdd, br_rowid
-from pds 
-where id = 'MTSY601POS'
-'''
-
-brsql.consulta_bd(query2)
-brsql.tabula()
-
-query3 = '''select distinct
-idtdd
-from pdd
-'''
-
-nome_do_arquivo = 'lista_de_pontos_siemens.csv'
-file_path = os.environ['SAGE'] + '/' + nome_do_arquivo
-
-with open(file_path, 'write') as f:
-    f.writelines(pontos_digitais)
+brsql.consulta_bd(query_comandos())
+print('Comandos Configurados :')
+comandos = brsql.tabula()
+# gera_arquivo(pontos_digitais, 'Template_de_comandos.csv')
